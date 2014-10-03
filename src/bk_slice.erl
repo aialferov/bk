c50ccf2fbde1f3348942d667845855375aa14b90
Anton I Alferov <casper.zx@gmail.com>
2014-10-03 19:12:30 -0400

    Add slicing by substring.

--- src/bk_slice.erl
+++ src/bk_slice.erl
@@ -13,8 +13,9 @@ read(Data, Args) -> lists:foldl(fun({ReadKey, Process, FilterKey}, NewData) ->
 end, Data, rules()).
 
 rules() -> [
-	{day, fun(Days) -> [list_to_integer(Day) || Day <- Days] end, days},
-	{group, fun(Groups) -> [list_to_atom(Group) || Group <- Groups] end, groups}
+	{day, fun(Days) -> [list_to_integer(D) || D <- Days] end, days},
+	{group, fun(Groups) -> [list_to_atom(G) || G <- Groups] end, groups},
+	{string, fun(Strings) -> Strings end, strings}
 ].
 
 filter(days, Data, Days) -> [
@@ -24,4 +25,14 @@ filter(days, Data, Days) -> [
 filter(groups, Data, Groups) -> [{Day, [
 	X || X = {Group, _} <- DataGroups,
 	Groups == [] orelse lists:member(Group, Groups)
-]} || {Day, DataGroups} <- Data].
+]} || {Day, DataGroups} <- Data];
+
+filter(strings, Data, Strings) -> [{Day, [{Group, [
+	X || X = {Name, _} <- Items,
+	Strings == [] orelse in_string(Name, Strings)
+]} || {Group, Items} <- Groups]} || {Day, Groups} <- Data].
+
+in_string(Str, Subs) -> lists:foldl(fun
+	(_, true) -> true;
+	(Sub, false) -> string:str(Str, Sub) > 0
+end, false, Subs).
